a447273e8e7dbb1310b210a2287150ddf8a41111
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-11-04 21:57:46 +0100

    "components folder created"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 const HomePage = () => {
   return (
-    <div>home page</div>
+    <div>
+      <h1 className="text-3xl text-red-400">Home page</h1>
+       <Button variant="outline" size="lg" className="capitalize " >click me</Button>
+    </div>
+      
   )
 }
 
